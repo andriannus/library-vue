8588559e78d5fed7ec1584801e7b5b917bff4ea4
Andre Simamora <andriansimamora@gmail.com>
2018-11-19 13:06:56 +0700

    Update frontend

--- client/src/views/user/ProfileUser.vue
+++ client/src/views/user/ProfileUser.vue
@@ -14,22 +14,66 @@
 
           <v-card-text>
             <div class="text-xs-center mb-4">
-              <v-avatar
-                class="avatar"
-                color="grey"
-                size="100"
-              >
-                <span class="white--text headline">
-                  {{ firstCharacter }}
-                </span>
-              </v-avatar>
+              <v-tooltip bottom>
+                <v-avatar
+                  class="avatar"
+                  color="grey"
+                  size="100"
+                  slot="activator"
+                >
+                  <span class="white--text headline">
+                    {{ firstCharacter }}
+                  </span>
+                </v-avatar>
+                <span>Change Picture</span>
+              </v-tooltip>
             </div>
 
-            <strong>Name</strong>
-            <p>{{ user.name }}</p>
+            <v-hover v-if="!isEditName">
+              <v-card
+                slot-scope="{ hover }"
+                :class="`elevation-${hover ? 2 : 0} mb-4`"
+                @dblclick="isEditName = true"
+              >
+                <v-card-text class="card-hover pa-2">
+                  <strong>Name</strong>
+                  <p class="mb-0">{{ user.name }}</p>
+                </v-card-text>
+              </v-card>
+            </v-hover>
 
-            <strong>Username</strong>
-            <p>{{ user.username }}</p>
+            <v-text-field
+              box
+              name="name"
+              label="Name"
+              v-model="user.name"
+              append-icon="mdi-close"
+              @click:append="isEditName = !isEditName"
+              v-if="isEditName"
+            ></v-text-field>
+
+            <v-hover v-if="!isEditUsername">
+              <v-card
+                slot-scope="{ hover }"
+                :class="`elevation-${hover ? 2 : 0}`"
+                @dblclick="isEditUsername = true"
+              >
+                <v-card-text class="card-hover pa-2">
+                  <strong>Username</strong>
+                  <p class="mb-0">{{ user.username }}</p>
+                </v-card-text>
+              </v-card>
+            </v-hover>
+
+            <v-text-field
+              box
+              name="username"
+              label="Username"
+              v-model="user.username"
+              append-icon="mdi-close"
+              @click:append="isEditUsername = !isEditUsername"
+              v-if="isEditUsername"
+            ></v-text-field>
           </v-card-text>
         </v-card>
       </v-flex>
@@ -40,11 +84,17 @@
 <style scoped>
 .avatar:hover {
   cursor: pointer;
+  opacity: 0.5;
+}
+
+.card-hover {
+  cursor: pointer;
 }
 </style>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import _ from 'lodash';
 
 @Component({
   metaInfo: {
@@ -53,6 +103,8 @@ import { Component, Vue } from 'vue-property-decorator';
 })
 
 export default class ProfileUser extends Vue {
+  private isEditName = false;
+  private isEditUsername = false;
   private user = {} as any;
 
   private mounted() {
